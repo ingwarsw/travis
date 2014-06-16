b254dd8640ae0e2d10c17db5a61dc1026315ae0a
Karol Lassak <karol.lassak@coi.gov.pl>
2014-06-16 14:03:16 +0200

    Working version
    - Doing more or less whats needed

--- travis.py
+++ travis.py
@@ -1,80 +1,89 @@
 #!/usr/bin/env python
-import os, os.path, sys, getopt, xml.dom.minidom, subprocess
+import os, os.path, sys, string, getopt, xml.dom.minidom, subprocess
 
 # https://github.com/Prototik/HoloEverywhere-Addon-Roboguice/blob/0d343e0ddd0607b0c9319eb21fdc50d451019f42/.travis.py
 
 def main(argv):
-    mvn_fetch()
-    mvn_test()
-  
-    if os.environ["TRAVIS_SECURE_ENV_VARS"] == "true" && os.environ["SONATYPE_SNAPSHOT"] == "true":
+    mode = string.join(argv)
+    if mode == "--fetch":
+        mvn_fetch()
+    elif mode == "--test":
+        mvn_test()
+    elif mode == "--deploy" and os.environ["TRAVIS_SECURE_ENV_VARS"] == "true" and os.environ["SONATYPE_SNAPSHOT"] == "true":
         mvn_deploy();
+    else:
+        print " # [ERR] No such mode"
 
-def mvn_call(tests = false, extra = [])
+def mvn_call(tests = False, extra = []):
     args = ["mvn", "install", "--batch-mode"]
     if not tests:
         args = args + ["-DskipTests=true"]
     call(args + extra)
-	    
-def mvn_fetch(num):
-	print " # [MAVEN] Fetching all"
-	for i in range(0, num):
-	    try:
-	        mvn_call()
-	        break
+        
+def mvn_fetch(num = 3):
+    print " # [MAVEN] Fetching all"
+    for i in range(0, num + 1):
+        try:
+            mvn_call()
+            break
         except subprocess.CalledProcessError:
-            print " [MAVEN] Retrying fetch [" + i + "].."
-            pass
+            if i < num:
+                print " # [MAVEN] Retrying fetch [{}]..".format(i + 1)
+                pass
+            else:
+                raise
+
 
 def mvn_test():
-	print " # [MAVEN] Test..."
-	    mvn_call(true)
+    print " # [MAVEN] Test..."
+    mvn_call(True)
 
 def mvn_deploy():
-	print " # [MAVEN] Deploy..."
-	maven_config = os.getcwd() + "/.maven.xml"
-	create_maven_config(maven_config, os.environ["SONATYPE_USERNAME"], os.environ["SONATYPE_PASSWORD"])
-	mvn_call(["deploy", "--settings=" + maven_config])
+    print " # [MAVEN] Deploy..."
+    maven_config = os.getcwd() + "/.maven.xml"
+    create_maven_config(maven_config, os.environ["SONATYPE_USERNAME"], os.environ["SONATYPE_PASSWORD"])
+    mvn_call(["deploy", "--settings=" + maven_config])
 
 def create_maven_config(filename, username, password):
-	m2 = xml.dom.minidom.parse(os.path.expanduser("~") + '/.m2/settings.xml')
+    m2 = xml.dom.minidom.parse(os.path.expanduser("~") + '/.m2/settings.xml')
 
-	settings = m2.getElementsByTagName("settings")[0]
-	serversNodes = settings.getElementsByTagName("servers")
-	if not serversNodes:
-		serversNode = m2.createElement("servers")
-		settings.appendChild(serversNode)
-	else:
-		serversNode = serversNodes[0]
+    settings = m2.getElementsByTagName("settings")[0]
+    serversNodes = settings.getElementsByTagName("servers")
+    if not serversNodes:
+        serversNode = m2.createElement("servers")
+        settings.appendChild(serversNode)
+    else:
+        serversNode = serversNodes[0]
 
-	serverNode = m2.createElement("server")
+    serverNode = m2.createElement("server")
 
-	serverId = m2.createElement("id")
-	serverId.appendChild(m2.createTextNode("sonatype-nexus-snapshots"))
-	serverNode.appendChild(serverId)
+    serverId = m2.createElement("id")
+    serverId.appendChild(m2.createTextNode("sonatype-nexus-snapshots"))
+    serverNode.appendChild(serverId)
 
-	serverUser = m2.createElement("username")
-	serverUser.appendChild(m2.createTextNode(username))
-	serverNode.appendChild(serverUser)
+    serverUser = m2.createElement("username")
+    serverUser.appendChild(m2.createTextNode(username))
+    serverNode.appendChild(serverUser)
 
-	serverPass = m2.createElement("password")
-	serverPass.appendChild(m2.createTextNode(password))
-	serverNode.appendChild(serverPass)
+    serverPass = m2.createElement("password")
+    serverPass.appendChild(m2.createTextNode(password))
+    serverNode.appendChild(serverPass)
  
-	serversNode.appendChild(serverNode)
+    serversNode.appendChild(serverNode)
   
-	f = open(filename, 'w')
-	f.write(m2.toxml())
-	f.close()
+    f = open(filename, 'w')
+    f.write(m2.toxml())
+    f.close()
 
 def call(command, hide_output=False):
-	if hide_output:
-		subprocess.check_call(command, env=os.environ, stdout=os.open("/dev/null", os.W_OK))
-	else:
-		subprocess.check_call(command, env=os.environ)
+    print " # [CALL] {}".format(string.join(command))
+    if hide_output:
+        subprocess.check_call(command, env=os.environ, stdout=os.open("/dev/null", os.W_OK))
+    else:
+        subprocess.check_call(command, env=os.environ)
 
 def call_output(command):
-	return subprocess.check_output(command, env=os.environ)
+    return subprocess.check_output(command, env=os.environ)
 
 def which(program):
     def is_exe(fpath):
